f03f7d5850153c381adf96dc1de394a17231b0ba
joshuabounds <jbounder@gmail.com>
2020-10-07 17:56:40 -0700

    Fleshed out the operators as well as network getting methods.

--- Python/Network2.py
+++ Python/Network2.py
@@ -5,6 +5,7 @@ Node syntax:
     OPP : CONNECT TO
     ----------------
     (-) : CLOSEST
+    (+) : FURTHEST
     (|) : CLOSEST AND FURTHEST
     (*) : ALL
 
@@ -32,7 +33,7 @@ Examples:
       |
     B-C-D
 
-    : (A,B,D)*C
+    : C+B+A+D
 
     A-B
     |X|
@@ -52,7 +53,7 @@ Examples:
       | |
       F-G
 
-    C-A-B|D-E-G-F
+    D|(C-A-B)+E-G|F
 
 """
 
@@ -61,6 +62,10 @@ from typing import *
 
 
 __all__ = 'Node',
+
+
+_T_OPP_S = Union['Node', List['Node'], Tuple['Node', ...]]
+_T_OPP_R = Tuple['Node', ...]
 
 
 class Node:
@@ -78,22 +83,136 @@ class Node:
     def __repr__(self):
         return repr(self.data)
 
-    def __sub__(self, other):
+    def __lshift__(self, other: 'Node') -> 'Node':
+        self.connections.add(other)
+        other.connections.add(self)
+        return self
+
+    def __add__(self, other: _T_OPP_S) -> _T_OPP_R:
         if isinstance(other, self.__class__):
-            self.connections.add(other)
-            other.connections.add(self)
+            self << other
             return self, other
         else:
-            self.connections.add(other[0])
-            other[0].connections.add(self)
+            self << other[-1]
             return (self,) + other
 
-    def __rsub__(self, other):
+    def __radd__(self, other: _T_OPP_S) -> _T_OPP_R:
         if isinstance(other, self.__class__):
-            self.connections.add(other)
-            other.connections.add(self)
+            self << other
             return other, self
         else:
-            self.connections.add(other[0])
-            other[0].connections.add(self)
+            self << other[0]
             return other + (self,)
+
+    def __sub__(self, other: _T_OPP_S) -> _T_OPP_R:
+        if isinstance(other, self.__class__):
+            self << other
+            return self, other
+        else:
+            self << other[0]
+            return (self,) + other
+
+    def __rsub__(self, other: _T_OPP_S) -> _T_OPP_R:
+        if isinstance(other, self.__class__):
+            self << other
+            return other, self
+        else:
+            self << other[-1]
+            return other + (self,)
+
+    def __or__(self, other: _T_OPP_S) -> _T_OPP_R:
+        if isinstance(other, self.__class__):
+            self << other
+            return self, other
+        else:
+            self << other[0] << other[-1]
+            return (self,) + other
+
+    def __ror__(self, other: _T_OPP_S) -> _T_OPP_R:
+        if isinstance(other, self.__class__):
+            self << other
+            return other, self
+        else:
+            self << other[0] << other[-1]
+            return other + (self,)
+
+    def __mul__(self, other: _T_OPP_S) -> _T_OPP_R:
+        if isinstance(other, self.__class__):
+            self << other
+            return self, other
+        else:
+            for n in other:
+                self << n
+            return (self,) + other
+
+    def __rmul__(self, other: _T_OPP_S) -> _T_OPP_R:
+        if isinstance(other, self.__class__):
+            self << other
+            return self, other
+        else:
+            for n in other:
+                self << n
+            return other + (self,)
+
+    def get_network(self) -> Set['Node']:
+        """
+        Returns all nodes in the network.
+
+        :return:
+            Set of all nodes in the network.
+        """
+
+        def recurse(cl, _a=set()):
+            _a |= cl
+            nl = {c for n in cl for c in n.connections} - _a
+            return recurse(nl) if nl else _a
+
+        return recurse({self})
+
+    def get_network_propagation(self):
+        """
+        Returns all nodes in the network ordered by distance from self.
+
+        :return:
+             List of sets, where each set represents another connection
+             level moving away from self.
+        """
+
+        def recurse(cl, _a=set()):
+            _a |= cl
+            nl = {c for n in cl for c in n.connections} - _a
+            return (cl,) + recurse(nl) if nl else (cl,)
+
+        return recurse({self})
+
+
+if __name__ == '__main__':
+
+    N = Node
+
+    a, b, c, d = N('A') - N('B') - N('C') - N('D')
+
+    # for n in a.get_network_propagation():
+    #     print(n)
+
+    # for n in a.get_network():
+    #     print(n, n.connections)
+
+    # for n in N('A')-N('B')-N('C'):
+    #     print(n, n.connections)
+    #
+    # print('')
+    # for n in N('A')-(N('B')-N('C')):
+    #     print(n, n.connections)
+    #
+    # print('')
+    # for n in N('A')+N('B')+N('C'):
+    #     print(n, n.connections)
+    #
+    # print('')
+    # for n in N('A')+(N('B')+N('C')):
+    #     print(n, n.connections)
+    #
+    # print('')
+    # for n in N('A')-(N('B'), N('C'), N('D')):
+    #     print(n, n.connections)
