6a831e9090b990306edb1da008019531ee2b90b5
joshuabounds <jbounder@gmail.com>
2020-10-21 17:58:53 -0700

    Added (&) operator

--- Python/Network2.py
+++ Python/Network2.py
@@ -1,66 +1,13 @@
 """
 Network syntax:
 
-    OPP : CONNECT TO
-    ----------------
-    (-) : CLOSEST
-    (+) : FURTHEST
-    (^) : CLOSEST AND FURTHEST
-    (|) : ALL
-
-Examples:
-
-    A-B
-    |
-    C
-
-    : C-A-B
-
-    A-B
-    |/
-    C
-
-    : A-B^C
-
-    A-B
-    |/|
-    C-D
-
-    : B-A^C^D
-
-      A
-      |
-    B-C-D
-
-    : C+B+A+D
-
-    A-B
-    |X|
-    C-D
-
-    : A-B^C|D
-
-    A-B-C
-    | | |
-    D-E-F
-
-    B-A-D^E-F^C
-
-    A-B
-    | |
-    C-D-E
-      | |
-      F-G
-
-    D^(C-A-B)+E-G^F
-
-      A
-     /|\
-    B C D
-     \|/
-      E
-
-    ?
+    OOO : OPP : CONNECT TO
+    ----------------------------
+    1   : (-) : CLOSEST
+    1   : (+) : FURTHEST
+    2   : (&) : NOTHING
+    3   : (^) : CLOSEST AND FURTHEST
+    4   : (|) : ALL
 
 """
 
@@ -86,13 +33,13 @@ class Node:
     def __repr__(self) -> str:
         return f'Node(' + repr(self.data) + ')'
 
-    def __sub__(self, other: 'Node') -> 'Node':
+    def __lshift__(self, other: 'Node') -> 'Node':
         self.connections.add(other)
         other.connections.add(self)
         return self
 
 
-class Network:
+class NodeContainer:
 
     _nodes: Tuple[Node] = None
 
@@ -109,51 +56,58 @@ class Network:
         nodes = ', '.join(repr(n.data) for n in self._nodes)
         return 'Network(' + nodes + ')'
 
-    def __sub__(self, other: 'Network') -> 'Network':
+    def __sub__(self, other: 'NodeContainer') -> 'NodeContainer':
         if self._nodes and other._nodes:
-            self._nodes[-1] - other._nodes[0]
+            self._nodes[-1] << other._nodes[0]
         return self.__class__(*self._nodes + other._nodes)
 
-    def __add__(self, other: 'Network') -> 'Network':
+    def __add__(self, other: 'NodeContainer') -> 'NodeContainer':
         if self._nodes and other._nodes:
-            self._nodes[0] - other._nodes[-1]
+            self._nodes[0] << other._nodes[-1]
         return self.__class__(*self._nodes + other._nodes)
 
-    def __xor__(self, other: 'Network') -> 'Network':
-        if self._nodes and other._nodes:
-            self._nodes[-1] - other._nodes[0]
-            self._nodes[0] - other._nodes[-1]
+    def __and__(self, other: 'NodeContainer') -> 'NodeContainer':
         return self.__class__(*self._nodes + other._nodes)
 
-    def __or__(self, other: 'Network') -> 'Network':
+    def __xor__(self, other: 'NodeContainer') -> 'NodeContainer':
+        if self._nodes and other._nodes:
+            self._nodes[-1] << other._nodes[0]
+            self._nodes[0] << other._nodes[-1]
+        return self.__class__(*self._nodes + other._nodes)
+
+    def __or__(self, other: 'NodeContainer') -> 'NodeContainer':
         for node_a in self._nodes:
             for node_b in other._nodes:
-                node_a - node_b
+                node_a << node_b
         return self.__class__(*self._nodes + other._nodes)
 
-    def __isub__(self, other: 'Network') -> 'Network':
+    def __isub__(self, other: 'NodeContainer') -> 'NodeContainer':
         if self._nodes and other._nodes:
-            self._nodes[-1] - other._nodes[0]
+            self._nodes[-1] << other._nodes[0]
         self._nodes += other._nodes
         return self
 
-    def __iadd__(self, other: 'Network') -> 'Network':
+    def __iadd__(self, other: 'NodeContainer') -> 'NodeContainer':
         if self._nodes and other._nodes:
-            self._nodes[0] - other._nodes[-1]
+            self._nodes[0] << other._nodes[-1]
         self._nodes += other._nodes
         return self
 
-    def __ixor__(self, other: 'Network') -> 'Network':
-        if self._nodes and other._nodes:
-            self._nodes[-1] - other._nodes[0]
-            self._nodes[0] - other._nodes[-1]
+    def __iand__(self, other: 'NodeContainer') -> 'NodeContainer':
         self._nodes += other._nodes
         return self
 
-    def __ior__(self, other: 'Network') -> 'Network':
+    def __ixor__(self, other: 'NodeContainer') -> 'NodeContainer':
+        if self._nodes and other._nodes:
+            self._nodes[-1] << other._nodes[0]
+            self._nodes[0] << other._nodes[-1]
+        self._nodes += other._nodes
+        return self
+
+    def __ior__(self, other: 'NodeContainer') -> 'NodeContainer':
         for node_a in self._nodes:
             for node_b in other._nodes:
-                node_a - node_b
+                node_a << node_b
         self._nodes += other._nodes
         return self
 
@@ -161,16 +115,91 @@ class Network:
     def nodes(self) -> Tuple[Node]:
         return self._nodes
 
-    def print_connections(self) -> NoReturn:
+    def clear_connections(self) -> NoReturn:
         for n in self._nodes:
-            print(str(n) + ' -> ' + ', '.join(map(str, n.connections)))
+            n.connections.clear()
+
+    def print_connections(self):
+        lines = (
+            str(node) + ' -> ' + ', '.join(map(str, node.connections))
+            for node in self._nodes
+        )
+        print('\n'.join(lines))
 
 
 if __name__ == '__main__':
 
-    A, B, C, D, E, F, G, H = map(Network, 'ABCDEFGH')
+    A, B, C, D, E, F, G, H = map(NodeContainer, 'ABCDEFGH')
 
-    # A - B
-    # |   |
-    # C - D
-    (A - B - C ^ D).print_connections()
+    # A-B
+    # |/
+    # C
+    N = A-B ^ C
+    print('')
+    N.print_connections()
+    N.clear_connections()
+
+    # A-B
+    # | |
+    # C-D
+    N = A-B-C ^ D
+    print('')
+    N.print_connections()
+    N.clear_connections()
+
+    # A-B
+    # |/|
+    # C-D
+    N = B-A ^ C ^ D
+    print('')
+    N.print_connections()
+    N.clear_connections()
+
+    # A-B
+    # |X|
+    # C-D
+    N = B-A ^ C | D
+    print('')
+    N.print_connections()
+    N.clear_connections()
+
+    # A-B-C
+    # | | |
+    # D-E-F
+    N = B-A-D ^ E ^ F-C
+    print('')
+    N.print_connections()
+    N.clear_connections()
+
+    # A-B
+    # | |
+    # C-D-E
+    #   | |
+    #   F-G
+    N = (D ^ B-A-C) + E-G ^ F
+    print('')
+    N.print_connections()
+    N.clear_connections()
+
+    #   A
+    #  /|\
+    # B C D
+    #  \|/
+    #   E
+    N = A & E | B & C & D
+    print('')
+    N.print_connections()
+    N.clear_connections()
+
+    #   A
+    #  /|\
+    # B C D
+    # | | |
+    # E F G
+    #  \|/
+    #   H
+    # N = B-E & C-F & D-G
+    N = A-B-E+C-F
+    print('')
+    N.print_connections()
+    N.clear_connections()
